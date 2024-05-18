575e38b3d7225ee2d54004c0a3e167c191c4fd57
kenjib2 <kenjib@rocketmail.com>
2024-05-18 09:06:57 -0700

    deleted comments

--- Start.cpp
+++ Start.cpp
@@ -10,11 +10,6 @@ KiwiSynth* kiwiSynth;
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
 	kiwiSynth->Process(out, size);
-	/*for (size_t i = 0; i < size; i++)
-	{
-		//out[0][i] = in[0][i];
-		//out[1][i] = in[1][i];
-	}*/
 }
 
 int main(void)
