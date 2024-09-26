43b4f2098ceb33dff14e0aa74bebefb531fd8dda
kenjib2 <kenjib@rocketmail.com>
2024-09-25 22:08:26 -0700

    Initialize storage, writepatchgroup, readpatch.

--- KiwiSynth/Patch/Storage.cpp
+++ KiwiSynth/Patch/Storage.cpp
@@ -8,6 +8,24 @@ namespace kiwi_synth
         InitializeIfNeeded();
     }
 
+    void Storage::InitializeIfNeeded() {
+        char checkCode[32];
+        ReadValidationCode(checkCode);
+        if (strcmp(checkCode, VALIDATION_CODE) != 0) {
+            WriteValidationCode();
+            SavedPatch patchGroup[2];
+            Patch patch;
+            patch.DefaultSettings();
+            patch.Save(&patchGroup[0]);
+            patch.Save(&patchGroup[1]);
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 64; j++) {
+                    WritePatchGroup(i, j, patchGroup);
+                }
+            }
+        }
+    }
+
     // buffer must be 32 character length minimum
     void Storage::ReadValidationCode(char* buffer) {
         void* readPtr = qspi.GetData(VALIDATION_BASE_ADDRESS);
@@ -19,18 +37,25 @@ namespace kiwi_synth
         qspi.Write(VALIDATION_BASE_ADDRESS, VALIDATION_SIZE, (uint8_t*)VALIDATION_CODE);
     }
     
-    void Storage::InitializeIfNeeded() {
-        char checkCode[32];
-        ReadValidationCode(checkCode);
-        if (strcmp(checkCode, VALIDATION_CODE) != 0) {
-            WriteValidationCode();
-
-            // Write 8 banks of initialized patches, 4 at a time.
+    void Storage::WritePatchGroup(int bankNumber, int groupNumber, SavedPatch patches[2]) {
+        uint32_t writeAddr = PATCHES_BASE_ADDRESS + bankNumber * BANK_SIZE + groupNumber * PATCH_GROUP_SIZE;
+        qspi.Erase(writeAddr, writeAddr + PATCH_GROUP_SIZE - 1);
+        for (int i = 0; i < 2; i++) {
+            uint8_t bytes[sizeof(SavedPatch)];
+            memcpy(bytes, &(patches[i]), sizeof(SavedPatch));
+            qspi.Write(writeAddr + i * PATCH_SIZE, PATCH_WRITE_SIZE, bytes);
         }
     }
 
+    void Storage::ReadPatch(int bankNumber, int patchNumber, SavedPatch* patch) {
+        void* readPtr = qspi.GetData(PATCHES_BASE_ADDRESS + bankNumber * BANK_SIZE + patchNumber * PATCH_SIZE);
+        memcpy(patch, readPtr, sizeof(SavedPatch));
+    }
+
     void Storage::Test(char* buffer) {
-        ReadValidationCode(buffer);
+        SavedPatch patch;
+        ReadPatch(3, 120, &patch);
+        strcpy(buffer, patch.name);
     }
 
 } // namespace kiwi_synth
--- KiwiSynth/Patch/Storage.h
+++ KiwiSynth/Patch/Storage.h
@@ -13,38 +13,28 @@ namespace kiwi_synth
 {
     const static uint32_t QSPI_BASE_ADDRESS = 0x90100000;
     const static uint32_t PATCHES_BASE_ADDRESS = QSPI_BASE_ADDRESS;
-    const static uint32_t VALIDATION_BASE_ADDRESS = QSPI_BASE_ADDRESS + 1048576;
-    const static char VALIDATION_CODE[32] = "BOMBASTIC_AUDIO_KIWI___00.00.03"; // 32 characters including terminating null at the end
+    const static uint32_t VALIDATION_BASE_ADDRESS = QSPI_BASE_ADDRESS + 2097152;
+    const static char VALIDATION_CODE[32] = "BOMBASTIC_AUDIO_KIWI___00.00.01"; // 32 characters including terminating null at the end
     const static int VALIDATION_SIZE = sizeof(VALIDATION_CODE); // 32;
-    const static int PATCH_SIZE = 1024; // Storage set aside for one patch
-    const static int PATCH_PAGE = 4096; // Minimum eraseable size for flash
+    const static int PATCH_SIZE = 2048; // Storage set aside for one patch
+    const static int PATCH_WRITE_SIZE = 1228; // Actual write size for one patch
+    const static int PATCH_GROUP_SIZE = 4096; // Storage set aside for a group of four patches, and minimum eraseable size for flash
+    const static int BANK_SIZE = 262144; // Storage set aside for 32 groups, or 128 patches.
 
     /*
      * Storage addresses:
-     * Validation Code: 0 to 31 -- will use 256 for block alignment
-     * Patch Previews: (16 + 4) * 512 = 10240 = 256 to 10495 OR BUILD THIS DYNAMICALLY FROM SAVED PATCHES TO LESSEN RE-WRITES OR DON'T NEED AT ALL?
-     * Saved Patch: 615 each -- will use 768 for 3 blocks. 256 + 768 * n to 256 to 768 * (n + 1). Could also store blocks of 4 or 5 each in 4096 byte sections to limit erasures.
-     * End of data: 256 + 768 * 512 = 393472
-     * 
-     * Single SavedPatch data size: 21 for vars + 99 for bools + 99 for int8_ts + 396 for floats = 615
-     *   
-     * 
-     * 
-     * 
-     * Saved Patch: 615 but will pad out to 1024 bytes total
-     * Each 4 patches is 4096 -- the minimum required erase. This limits erase/write to only 4 patches at a time and extends flash memory lifespan. (100,000 erases)
-     * 
-     * Each bank is 128k, or 32 groups of 4 patches each
-     *      0 - 131071: Patch bank 1
-     * 131072 - 262143: Patch bank 2
-     * 262144 - 393215: Patch bank 3
-     * 393216 - 524287: Patch bank 4
-     * 524288 - 655359: Patch bank 5
-     * 655360 - 786431: Patch bank 6
-     * 786432 - 917503: Patch bank 7
-     * 917504 - 1048575: Patch bank 8
-     * 
-     * 1048576: 32 character validation/version code.
+     * Single SavedPatch data size: 1228 but will pad out to 2048 bytes total
+     * Each 2 patches is 4096 -- the minimum required erase. This limits erase/write to only 2 patches at a time and extends flash memory lifespan. (100,000 erases)
+     * Each bank is 256k, or 64 groups of 2 patches each
+     *       0 -  262143: Patch bank 1
+     *  262144 -  524287: Patch bank 2
+     *  524288 -  786431: Patch bank 3
+     *  786432 - 1048575: Patch bank 4
+     * 1048576 - 1310719: Patch bank 5
+     * 1310720 - 1572863: Patch bank 6
+     * 1572864 - 1835007: Patch bank 7
+     * 1835008 - 2097151: Patch bank 8
+     * 2097152: 32 character validation/version code.
      */
     class Storage
     {
@@ -54,6 +44,8 @@ namespace kiwi_synth
             void InitializeIfNeeded();
             void ReadValidationCode(char* buffer);
             void WriteValidationCode();
+            void WritePatchGroup(int bankNumber, int groupNumber, SavedPatch patches[4]);
+            void ReadPatch(int bankNumber, int patchNumber, SavedPatch* patch);
 
         public:
             void Init(QSPIHandle qspi);
